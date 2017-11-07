7c977254c3cb063be12791cc8b44f2773cbf6baa
Andrew Anelli <ananelli@e1z3r7p22.42.us.org>
2017-11-07 10:47:50 -0800

    latest build

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ananelli <ananelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 15:51:08 by ananelli          #+#    #+#             */
-/*   Updated: 2017/11/07 06:42:57 by ananelli         ###   ########.fr       */
+/*   Updated: 2017/11/07 08:23:42 by ananelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	c = 0;
 	while (s1[c] == s2[c] && s1[c] && s2[c] && c < (int)n)
 		c++;
+	if (c == (int)n)
+		c--;
 	return ((unsigned char)s1[c] - (unsigned char)s2[c]);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: ananelli <ananelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 15:42:35 by ananelli          #+#    #+#             */
-/*   Updated: 2017/11/07 06:56:35 by ananelli         ###   ########.fr       */
+/*   Updated: 2017/11/07 10:42:32 by ananelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,9 @@
 
 int	ft_strnequ(const char *s1, const char *s2, size_t n)
 {
-	int c;
-
-	c = -1;
-	if (s1 && s2 && n != 0)
-	{
-		while ((int)n >= ++c && s1[c] && s2[c])
-		{
-			if (s1 == s2 && s1 == '\0' && s2 == '\0')
-				return (1);
-		}
-	}
-	if (s1 == s2 && n != 0)
+	if (!s1 || !s2)
+		return (0);
+	else if (ft_strncmp(s1, s2, n) == 0)
 		return (1);
 	return (0);
 }
-//
-// int main(int ac, char **av)
-// {
-// 	printf("%d\n", ft_strnequ("ededeqdf", "", 0));
-// 	return(0);
-// }
