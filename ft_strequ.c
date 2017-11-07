f2b7c263a8aa1ee51c0b9157ecdb30303a024214
Andrew Anelli <ananelli@e1z3r7p22.42.us.org>
2017-11-07 11:25:31 -0800

    ooops

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,9 +6,11 @@
 /*   By: ananelli <ananelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 15:33:14 by ananelli          #+#    #+#             */
-/*   Updated: 2017/11/07 11:18:31 by ananelli         ###   ########.fr       */
+/*   Updated: 2017/11/07 11:24:16 by ananelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strequ(char const *s1, char const *s2)
 {
